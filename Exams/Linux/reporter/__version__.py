424cdc3566207ac9a8057d7be7ac8239d99ebe7b
Yaakov Azat <yaakovazat@gmail.com>
2020-04-18 15:16:20 +0600

    added automatic report generator

---
+++ Exams/Linux/reporter/__version__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#                            _                _____
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#
+#
+
+__title__ = "reporter"
+__description__ = "UniSat Exam Results Reporter"
+__url__ = "https://unisat.kz"
+__version__ = "1.0.2"
+__author__ = "Yaakov Azat"
+__author_email__ = "yaakovazat@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 Azat Artificial Intelligence, LLP (AzatAI)"
